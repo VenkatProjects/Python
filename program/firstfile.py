e03079cbff488c6e445011ed2dec4b7c73c3d9b2
root <root@ip-172-31-41-144.ap-northeast-1.compute.internal>
2023-01-27 06:07:06 +0000

    Created and added the basic py file

---
+++ program/firstfile.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+#taking the input value and print
+print("............................................\n")
+
+print("Taking the input and printing\n")
+
+name = input("Enter the name: ")
+print("\n")
+print( "Hey " + name)
+
+#concatinating with string
+
+print("-------------------------------------------\n")
+
+print("Concatinating the two string\n")
+
+a = input("Enter the first string: ")
+b = input("Enter the second string: ")
+print("\n")
+print("concatinating valus is: " + a +" " + b)
+
+print("-------------------------------------------\n")
+
+#concatinating the string and int value
+
+print("Concatinating the string and int\n")
+
+a = str(input("Enter your age: "))
+print("\n")
+
+print ( "Hello,I'm " + a + " age")
+
+print("-------------------------------------------\n")
+
+#List the values 
+
+#Slicing the string values
+
+#sequence of lists
+
+
+
