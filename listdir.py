b47eacc12ede9ea6cbad3bad184451bfdfe3060d
root <root@ip-172-31-41-144.ap-northeast-1.compute.internal>
2023-01-27 05:10:52 +0000

    modified the listdr file and filename

---
+++ listdir.py
@@ -0,0 +1,14 @@
+
+#Listing the directory files
+
+from os import listdir
+
+print("\nList the files from 'data' directory\n")
+
+print("----------------------------------------\n")
+
+print(listdir('./data'))
+
+print("\n")
+
+print("-----------------------------------------\n")
